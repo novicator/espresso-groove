bde3d4a5d225bd1a8450f55674c6ab516d5274ec
Nova <031103ak@Nova.local>
2026-03-30 08:39:35 -0400

    save changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,8 +132,8 @@ export default function Home() {
             className="flex flex-col"
             style={{
               paddingLeft: '4vw',
-              paddingTop: '8vw',
-              gap: '5vw',
+              paddingTop: '6vw',
+              gap: '3vw',
             }}
           >
             <Link
@@ -212,6 +212,26 @@ export default function Home() {
                 Merch
               </span>
             </Link>
+
+            <Link
+              href="/events"
+              className="rounded-full bg-[#24ADFF] noisy flex items-center justify-center active:scale-125 duration-150 transition-all"
+              style={{ width: '43vw', height: '13vw', gap: '2vw', border: '2px solid #3a8abf' }}
+            >
+              <svg style={{ width: '6vw', height: '6vw', filter: 'drop-shadow(0 0 6px rgba(0,0,0,1))' }} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 9H21M7 3V5M17 3V5M6 13H8M6 17H8M11 13H13M11 17H13M16 13H18M16 17H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+              <span
+                className="font-[family-name:var(--font-libre-baskerville)] font-bold uppercase text-white"
+                style={{
+                  textShadow: '1px 1px 0 rgba(255,255,255,0.4), -1px -1px 0 rgba(0,0,0,0.4), 0 0 8px rgba(0,0,0,1)',
+                  letterSpacing: '0.12em',
+                  fontSize: '4.6vw',
+                }}
+              >
+                Events
+              </span>
+            </Link>
           </div>
 
           {/* Mug */}
@@ -222,7 +242,7 @@ export default function Home() {
             style={{
               width: '51vw',
               right: '0vw',
-              top: '98vw',
+              top: '103vw',
               filter: 'drop-shadow(0px 0px 15px rgba(255,150,50,0.5))',
             }}
           />
